b1165d8de1fd9334134e0ab503319cfbd64b3ec6
uwol-is-june <std06158@naver.com>
2026-05-06 22:25:06 +0900

    chore: /test1 테스트 페이지 추가

---
+++ app/test1/page.tsx
@@ -0,0 +1,7 @@
+export default function Test1Page() {
+  return (
+    <main className="flex flex-col items-center justify-center flex-1 px-4">
+      <p className="text-[var(--text-sub)]">test1</p>
+    </main>
+  )
+}
